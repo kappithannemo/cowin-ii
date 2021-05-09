0a45f4117cb445f1c499c13553b2d97c4ab9eb2c
Vijay Jain <mnnit.vijay@gmail.com>
2021-05-09 10:22:13 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ module.exports = {
     },
     whatsapp: '+919999999999',  // whatsapp should be same number which you used to setup twilio sandbox, append +91
     pincodes: [302017], // Pincodes where slots are needed to be found
-    districts: [['Rajasthan', 'Jaipur I'], ['Rajasthan', 'Jaipur II']], // State and district, you can choose to specify anyone or both out of pincodes and districs.
+    districts: [['Rajasthan', 'Jaipur I'], ['Rajasthan', 'Jaipur II']], // State and district, you can choose to specify anyone or both out of pincodes and districts.
     age: 18,    // Age limit, 18 or 45
 }
