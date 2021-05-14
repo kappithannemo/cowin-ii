28be0270c1b208216d898cf91cc18f4e92527a2e
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-05-14 19:35:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ module.exports = {
     age: 18,    // Age limit, 18 or 45
     telegram: {     // Refer https://gist.github.com/mraaroncruz/e76d19f7d61d59419002db54030ebe35
         token: '1836312473:AAHrs5LLQrqk57BXJEa9yBg7tbQVkP-uuDw',
-        channelId: '1001264121416',
+        channelId: '-1001264121416',
     }
 }
