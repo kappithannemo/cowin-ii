86a979960bdf751cbf420f178a7f5dc79ebb835f
kappithannemo <78903191+kappithannemo@users.noreply.github.com>
2021-05-14 19:31:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -2,11 +2,11 @@ module.exports = {
     /**
      * You can poll for multiple pincodes and districts as per your needs.
      */
-    pincodes: [301001], // Pincodes to poll
-    districts: [['Rajasthan', 'Alwar'], ['Rajasthan', 'Jaipur I'], ['Rajasthan', 'Jaipur II']], // State and district to poll.
+    pincodes: [671531], // Pincodes to poll
+    districts: [['Kerala', 'Kasaragod'], ['Kerala', 'Kannur'], ['Kerala', 'Malappuram']], // State and district to poll.
     age: 18,    // Age limit, 18 or 45
     telegram: {     // Refer https://gist.github.com/mraaroncruz/e76d19f7d61d59419002db54030ebe35
-        token: '',
-        channelId: '',
+        token: '1836312473:AAHrs5LLQrqk57BXJEa9yBg7tbQVkP-uuDw',
+        channelId: '1001264121416',
     }
 }
